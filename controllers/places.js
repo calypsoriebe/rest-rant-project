5d748adcb46c693d60ee897f6c84f48758d34ead
Calypso Kay Riebe <calypso@Calypsos-MBP.attlocal.net>
2022-07-10 10:54:31 -0400

    form data working

--- controllers/places.js
+++ controllers/places.js
@@ -7,6 +7,7 @@ router.get("/new", (req, res) => {
   res.render("places/new");
 });
 router.post("/", (req, res) => {
+  console.log(req.body);
   res.send("POST /places");
 });
 module.exports = router;
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
 //controllers
 app.use("/places", require("./controllers/places"));
 
